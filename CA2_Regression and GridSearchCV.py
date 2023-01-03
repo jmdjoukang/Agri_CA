702ea19992d0f352ddc22ebf53950cf51eae21a7
Jean-Marie TD <120069188+jmdjoukang@users.noreply.github.com>
2023-01-03 04:31:39 +0000

    Add files via upload

--- CA2_Regression and GridSearchCV.py
+++ CA2_Regression and GridSearchCV.py
@@ -240,6 +240,98 @@ print(XGBRGS.best_params_)
 print(XGBRGS.best_estimator_)
 
 
+# In[1]:
+
+
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+
+def f(x):
+    """The function to predict."""
+    return x * np.sin(x)
+
+
+rng = np.random.RandomState(42)
+X = np.atleast_2d(rng.uniform(0, 10.0, size=1000)).T
+expected_y = f(X).ravel()
+
+
+# In[2]:
+
+
+sigma = 0.5 + X.ravel() / 10
+noise = rng.lognormal(sigma=sigma) - np.exp(sigma**2 / 2)
+y = expected_y + noise
+
+
+# In[3]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+
+# In[4]:
+
+
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.metrics import mean_pinball_loss, mean_squared_error
+
+
+all_models = {}
+common_params = dict(
+    learning_rate=0.05,
+    n_estimators=200,
+    max_depth=2,
+    min_samples_leaf=9,
+    min_samples_split=9,
+)
+for alpha in [0.05, 0.5, 0.95]:
+    gbr = GradientBoostingRegressor(loss="quantile", alpha=alpha, **common_params)
+    all_models["q %1.2f" % alpha] = gbr.fit(X_train, y_train)
+
+
+# In[5]:
+
+
+gbr_ls = GradientBoostingRegressor(loss="squared_error", **common_params)
+all_models["mse"] = gbr_ls.fit(X_train, y_train)
+
+
+# In[6]:
+
+
+xx = np.atleast_2d(np.linspace(0, 10, 1000)).T
+
+
+# In[7]:
+
+
+import matplotlib.pyplot as plt
+
+
+y_pred = all_models["mse"].predict(xx)
+y_lower = all_models["q 0.05"].predict(xx)
+y_upper = all_models["q 0.95"].predict(xx)
+y_med = all_models["q 0.50"].predict(xx)
+
+fig = plt.figure(figsize=(10, 10))
+plt.plot(xx, f(xx), "g:", linewidth=3, label=r"$f(x) = x\,\sin(x)$")
+plt.plot(X_test, y_test, "b.", markersize=10, label="Test observations")
+plt.plot(xx, y_med, "r-", label="Predicted median")
+plt.plot(xx, y_pred, "r-", label="Predicted mean")
+plt.plot(xx, y_upper, "k-")
+plt.plot(xx, y_lower, "k-")
+plt.fill_between(
+    xx.ravel(), y_lower, y_upper, alpha=0.4, label="Predicted 90% interval"
+)
+plt.xlabel("$x$")
+plt.ylabel("$f(x)$")
+plt.ylim(-10, 25)
+plt.legend(loc="upper left")
+plt.show()
+
+
 # In[ ]:
 
 
